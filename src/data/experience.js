06af65f2b4e25c2e9ed5cfb3920c19113a814a5b
Arath Velazquez <arath22@gmail.com>
2021-03-03 17:09:43 -0600

    Working on experience section.

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,58 +1,130 @@
 export const experience = [
   {
     companyName: 'Columbia',
+    webPage: {
+      url: 'https://columbia.com'
+    },
+    location: 'Portland, OR USA',
     category: ['professional'],
     role: 'Fullstack Dev & AWS Tech Lead',
+    stack: ['Node.js', 'AWS Lambda', 'AWS S3', 'AWS CloudFormation', 'AWS Dynamo', 'React.js', 'OKTA'],
+    image: ['experience/columbia.jpg'],
     period: 'Dec 2018 - Present',
     description: 'Accountable for implementation of microservices and RESTful APIs running in AWS with Lambda (Node.js), Dynamo, S3, CloudWatch, CloudFormation, SES, JWT, OKTA, Swagger and MongoDB (Atlas). <br /> Most recently, I have been involved in all the aspects of development and also provide production support, analysis, feature enhancements, mentoring, unit testing and code reviews. <br /> As Tech Lead, I work closely with the entire AWS team, Architects, Product Owners, Business Analysts, DevOps and SFCC team to integrate our endpoints, jobs and tasks'
   },
   {
     companyName: 'MediaHunter',
+    webPage: {
+      url: 'https://mediahunter.mx'
+    },
+    location: 'Jalisco, MX',
     category: ['professional', 'freelance', 'entrepreneur'],
     role: 'Partner & Fullstack Developer',
-    period: 'Dec 2015 - Present',
+    stack: ['Node.js', 'MongoDB', 'Angular', 'Elastic Search', 'Express.js', 'E-Commerce', 'Digital Ocean', 'Material Design', 'Mandrill'],
+    image: ['experience/mediahunter.jpg'],
+    period: 'Dec 2014 - Present',
     description: 'During this period, I worked for two different companies as freelance and then I started working fulltime for one of these. I was the main developer for a startup called MediaHunter and also I was working remotely as Node.js (MEAN) developer for a company based on Seattle and in February 2018 I started working fulltime with them. <br /> For both companies, I performed most of the development and created Ecommerce platforms with Node.js, Angular, Bootstrap, Material Design, ElasticSearch and MongoDB. I also performed all the implementation for database and web servers by using independent VPS and did the integration of different services and API\'s such as Google Maps, Mandrill for transactional emails, DigitalOcean, Google Cloud among others.'
   },
   {
     companyName: 'iTexico',
+    webPage: {
+      url: 'https://itexico.com'
+    },
+    location: 'Jalisco, MX',
     category: ['professional'],
     role: 'Fullstack Developer',
+    stack: ['Node.js', 'AWS', 'MongoDB', 'React.js', 'HTML & CSS', 'Express'],
+    image: ['experience/itexico.jpg'],
     period: 'Oct 2017 - Feb 2018',
     description: 'Worked as Full Stack for MediaShift (AdTech Platform Solution) to create a dashboard by using Mongo, Node.js and React.js stack.'
   },
   {
-    companyName: 'Hildebrando',
+    companyName: 'Global Hitss',
+    webPage: {
+      url: 'https://globalhitss.com/'
+    },
+    location: 'Jalisco, MX',
     category: ['professional'],
     role: 'Software Engineer',
+    stack: ['Shell Scripting', 'Node.js', 'Express.js', 'MongoDB', 'Google Maps API', 'Splunk'],
+    image: [],
     period: 'Aug 2012 - Oct 2017',
     description: 'Worked for 2 accounts during this time. For DexOne, I was involved in development for DexKnows and third parties services integration such Google Maps, OpenTable, among other API\'s. Worked for the first time with MongoDB only for comments section and also first time with Splunk and Apache Solr. For AT&T, I was part of the data team and worked for backend services and internal tools.'
   },
   {
     companyName: 'TCS',
+    webPage: {
+      url: 'https://www.tcs.com/worldwide'
+    },
+    location: 'Jalisco, MX',
     category: ['professional'],
     role: 'Unix Developer',
+    stack: ['Shell Scripting', 'Java', 'WebSphere',],
+    image: [],
     period: 'Jul 2011 - Aug 2012',
-    description: 'Worked as Unix Developer for Bank of America. <br /> Application development and support for backend, data migration and infrastructure. Working mainly with shell scripting, WebSphere and created internal applications.'
+    description: 'Worked as Unix Developer for <strong>Bank of America</strong>. <br /> Application development and support for backend, data migration and infrastructure. Working mainly with shell scripting, WebSphere and created internal applications.'
   },
   {
     companyName: 'Infosys',
+    webPage: {
+      url: 'https://www.infosys.com/'
+    },
+    location: 'Nuevo León, MX',
     category: ['professional'],
     role: 'Software Engineer',
+    stack: ['Shell Scripting', 'Configuration Management'],
+    image: [],
     period: 'Oct 2010 - Jul 2011',
     description: 'Worked for Nordstrom account providing application development mostly for Unix tools and support.'
   },
   {
     companyName: 'Dextra Technologies',
+    webPage: {
+      url: 'https://dextratech.com/'
+    },
+    location: 'Nuevo León, MX',
     category: ['professional'],
     role: 'Embedded Systems Engineer',
+    stack: ['Shell Scripting', 'SQL', 'Embedded Systems'],
+    image: [],
     period: 'Nov 2007 - Oct 2010',
     description: 'Worked for Texas Instruments, specifically with Linux team to flash and work with new features for OMAP3 and OMAP4 processors.'
   },
   {
     companyName: 'IBM',
+    webPage: {
+      url: 'https://www.ibm.com/mx-es'
+    },
+    location: 'Jalisco, MX',
     category: ['professional'],
     role: 'Software Developer',
+    stack: ['Shell Scripting', 'Z Series', 'Java'],
+    image: [],
     period: 'Jul 2006 - Jan 2007',
     description: 'Internship in last year of career. <br/> Member of the first pilot program to work directly with IBM for 6 months. IBM and Universidad de Colima had an agreement to integrate students to this new program and after some interviews from IBM employees, only 10 students were selected.'
   },
+  {
+    companyName: 'MeediaHub',
+    webPage: {
+      url: ''
+    },
+    category: ['entrepreneur', 'concepts'],
+    role: 'Founder and Software Developer',
+    stack: ['Node.js', 'MongoDB', 'Javascript', 'HTML5 & CSS3', 'D3.js', 'jQuery'],
+    image: [],
+    period: 'Jul 2015 - 2017 [Shut down]',
+    description: ''
+  },
+  {
+    companyName: 'Wordpress Plugins & Chrome Extensions',
+    webPage: {
+      url: 'https://chrome.google.com/webstore/detail/url-shortener-ushmx/nmodcgfljdebfnafgaeefglhcngpdjlj?hl=en&authuser=1'
+    },
+    category: ['concepts', 'just_for_fun', 'freelance'],
+    role: 'Software Developer',
+    stack: ['PHP', 'Javascript', 'SQL', 'HTML & CSS'],
+    image: ['imgs/ush.mx_1.png'],
+    period: '2011 - 2012 [Shut down]',
+    description: 'Created some plugins for Wordpress and some chrome extensions.  for a service I created called ush.mx (URL shortener)'
+  }
 ];
